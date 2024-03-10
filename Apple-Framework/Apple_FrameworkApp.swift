92a9554f5d19fe97d60029e58e517eab6117814f
Rushikesh-24 <rushikeshgaokar24@gmail.com>
2024-03-10 12:52:08 +0530

    Initial Commit

---
+++ Apple-Framework/Apple_FrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworkApp.swift
+//  Apple-Framework
+//
+//  Created by Rushikesh Gaonkar on 10/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
